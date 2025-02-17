2db925e3555e044db21f4a9601ab660630195fd2
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-17 16:02:15 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,52 +1,52 @@
 class script(object):
     HELP_TXT = """<b>Hey</b> {}
     
-<b>Here Is The Help For My Commands.</b>"""
+<b>Voici l'aide pour mes commandes.</b>"""
 
-    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    CAPTION_TXT = """<b><u>📝 Commment Ajouter un légende ?</u></b>
 
-<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
-<b>⦿ /see_caption - Use This Command To See Your Caption</b>
-<b>⦿ /del_caption - Use This Command To Delete Your Caption</b>"""
+<b>⦿ /set_caption -Utilise cette commande pour définir ta légende.</b>
+<b>⦿ /see_caption - utilise cette commande pour voir ta légende</b>
+<b>⦿ /del_caption - utilise cette commande pour supprimé ta légende</b>"""
    
-    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+    THUMBNAIL_TXT = """<b><u>🖼️ COMMENT METTRE UNE MINIATURE</u></b>
 
-<b>⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....</b>
+<b>⦿ Tu peux ajouter une miniature personnalisée simplement en m'envoyant une photo....</b>
 
-<b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
-<b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
+<b>⦿ /viewthumb -Utilise cette commande pour voir ta miniature.</b>
+<b>⦿ /delthumb -Utilise cette commande pour supprimer ta miniature.</b>"""
 
-    ABOUT_TXT = """<b>🤖 My Name :</b> {}
+    ABOUT_TXT = """<b>🤖 Mon Nom :</b> {}
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://telegram.me/TechifyBots'>Techify Bots</a>
-<b>🧑‍💻 Developer :</b> <a href='https://telegram.me/CallOwnerBot'>Rahul</a>
+<b>📢 Chaîne :</b> <a href='https://telegram.me/TechifyBots'>Techify Bots</a>
+<b>🧑‍💻 Developpeur :</b> <a href='https://telegram.me/Kingcey'>🇰ιηg¢єу</a>
 
-<b>♻️ Bot Made By :</b> @TechifyBots"""
+<b>♻️ créé par :</b> @BotZFlix"""
 
     DONATE_TXT = """
-<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+<b>🥲 Merci de montrer de l'intérêt pour le don ! ❤️</b>
 
-If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+Si tu aimes mes bots et mes projets, tu peux 🎁 me faire un don de n'importe quel montant, à partir de 500F jusqu'à ce que tu le souhaites.
 
-<b>🛍 UPI ID:</b> <code>TechifyBots@UPI</code> 
+<b>🛍 Contact:</b> <b>@Kingcey</b> 
 
-<b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
+<b>📸 Envoie une capture d'écran après le don pour activer ton plan premium.</b> """
 
-    ADMIN_TXT = """<b><u>🦋 ADMIN ALL COMMANDS HERE</u></b>
+    ADMIN_TXT = """<b><u>🦋 ADMIN TOUS LES COMMANDES ICI.</u></b>
 
-<b>⦿ /users - Use This Command To See Total Users</b>
-<b>⦿ /allids - Use This Command To See All Users IDs</b>
-<b>⦿ /broadcast - Use This Command To Send A Message To Users</b>
-<b>⦿ /warn - Use This Command To Send A Message To A User</b>
-<b>⦿ /resetpower - Use This Command To Reset User Power</b>
-<b>⦿ /ceasepower - Use This Command To Cease User Power</b>
-<b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
-<b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
+<b>⦿ /users - Utilise cette commande pour voir le nombre total d'utilisateurs.</b>
+<b>⦿ /allids -Utilise cette commande pour voir tous les ID des utilisateurs.</b>
+<b>⦿ /broadcast - Utilise cette commande pour envoyer un message aux utilisateurs</b>
+<b>⦿ /warn -Utilise cette commande pour envoyer un message à un utilisateur.</b>
+<b>⦿ /resetpower -Utilise cette commande pour réinitialiser la puissance de l'utilisateur.</b>
+<b>⦿ /ceasepower -Utilise cette commande pour cesser la puissance de l'utilisateur.</b>
+<b>⦿ /addpremium - Utilise cette commande pour ajouter le premium aux utilisateurs.</b>
+<b>⦿ /restart - Utilise cette commande pour annuler tous les processus et redémarrer le bot.</b>"""
 
-    METADATA_TXT = """<b><u>🖼️  HOW TO SET CUSTOM METADATA</u></b>
-For Example :-
-<code>By @TechifyBots</code>
-💬 For Any Help Contact @TechifySupport
+    METADATA_TXT = """<b><u>🖼️  METADONNER</u></b>
+PAr Example :-
+<code>Par @botZFlix</code>
+💬 Pour toute aide, contacte @BZFSupport.
 """
