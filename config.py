d984bcf5ebbb56d665f8b4ce3300935b9f9b4697
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-17 20:17:41 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "24817837"))
+API_HASH = os.environ.get("API_HASH", "acd9f0cc6beb08ce59383cf250052686")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7851825233:AAFaXypYR9v8EA6byt1GkgBXc1h893lTfHc")
+ADMIN = int(os.environ.get("ADMIN", "7428552084"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,13 +14,13 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "botzflix")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002376378205"))
 
 
 # Mongo DB Database Config
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "TechifyBots")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "botzflix")
 
 
 # Other Variables Config
