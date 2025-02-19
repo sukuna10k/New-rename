b37f66bc194550f63cb92e16c153c8aca8b2db87
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-19 18:34:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return "Ankit is great !!"
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
