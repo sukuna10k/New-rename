7c25fc12934874eef046762294bf2c85a94f1a6b
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-19 17:26:07 +0100

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -9,10 +9,10 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("<b><u>Give Me A Caption To Set</u></b>\n\n<b>Example :</b> <code>/set_caption 📕 Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}</code>")
+       return await message.reply_text("<b><u>Donne-moi une légende à définir</u></b>\n\n<b>Example :</b> <code>/set_caption 📕 Nom ➠ : {filename} \n\n🔗 Poids ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}</code>")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("Your Caption Successfully Added ✅")
+    await message.reply_text("Votre Légende a été ajouté ✅")
 
 
 
@@ -20,7 +20,7 @@ async def add_caption(client, message):
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("You Don't Have Any Custom Caption ❌")
+        await message.reply_text("Vous n'avez aucune légende ❌")
         return
     delcaption(int(message.chat.id))
     await message.reply_text("Your Caption Successfully Deleted 🗑️")
