e2cb9a5d5763bbfefe3fa751ec7168f3a6701072
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-28 17:16:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Ankit is great !!"
+def hello_world():
+    return 'Rahul'
+
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
