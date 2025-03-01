43225a4f46e3b48de49041616c42b1cfe8b739bc
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-03-01 13:03:57 +0000

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,87 +1,82 @@
-from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup)
-from pyrogram import Client , filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
 from script import *
 from config import *
 
-
-
-
-
 @Client.on_callback_query(filters.regex('about'))
-async def about(bot,update):
+async def about(bot, update):
     text = script.ABOUT_TXT.format(bot.me.mention)
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("🔙 Retour",callback_data = "home")]
-                  ])
-    await update.message.edit(text = text,reply_markup = keybord)
-
-
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔙 Retour", callback_data="home")]
+    ])
+    if update.message.text != text:
+        await update.message.edit_text(text=text, reply_markup=keybord)
 
 @Client.on_message(filters.private & filters.command(["donate"]))
-async def donatecm(bot,message):
+async def donatecm(bot, message):
     text = script.DONATE_TXT
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🦋 Admin",url = "https://telegram.me/Kingcey"),
-        InlineKeyboardButton("✖️ Annuler",callback_data = "cancel") ]])
-    await message.reply_text(text = text,reply_markup = keybord)    
-
-
+        [InlineKeyboardButton("🦋 Admin", url="https://telegram.me/Kingcey"),
+         InlineKeyboardButton("✖️ Annuler", callback_data="cancel")]
+    ])
+    await message.reply_text(text=text, reply_markup=keybord)
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["admin"]))
-async def admincm(bot,message):
+async def admincm(bot, message):
     text = script.ADMIN_TXT
     keybord = InlineKeyboardMarkup([
-        [InlineKeyboardButton("✖️ Close ✖️",callback_data = "cancel") ]])
-    await message.reply_text(text = text,reply_markup = keybord)    
-
-
+        [InlineKeyboardButton("✖️ Close ✖️", callback_data="cancel")]
+    ])
+    await message.reply_text(text=text, reply_markup=keybord)
 
 @Client.on_callback_query(filters.regex('help'))
-async def help(bot,update):
+async def help(bot, update):
     text = script.HELP_TXT.format(update.from_user.mention)
-    keybord = InlineKeyboardMarkup([ 
-                    [InlineKeyboardButton('🏞 Vignette', callback_data='thumbnail'),
-                    InlineKeyboardButton('✏ Légende', callback_data='caption')],
-                    [InlineKeyboardButton('🏠 Home', callback_data='home'),
-                    InlineKeyboardButton('💵 Don', callback_data='donate')]
-                   ])
-    await update.message.edit(text = text,reply_markup = keybord)
-
-
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton('🏞 Vignette', callback_data='thumbnail'),
+         InlineKeyboardButton('✏ Légende', callback_data='caption')],
+        [InlineKeyboardButton('🏠 Home', callback_data='home'),
+         InlineKeyboardButton('💵 Don', callback_data='donate')]
+    ])
+    if update.message.text != text:
+        await update.message.edit_text(text=text, reply_markup=keybord)
 
 @Client.on_callback_query(filters.regex('thumbnail'))
-async def thumbnail(bot,update):
+async def thumbnail(bot, update):
     text = script.THUMBNAIL_TXT
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("🔙 Retour",callback_data = "help")]
-		  ])
-    await update.message.edit(text = text,reply_markup = keybord)
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔙 Retour", callback_data="help")]
+    ])
+    if update.message.text != text:
+        await update.message.edit_text(text=text, reply_markup=keybord)
 
 @Client.on_callback_query(filters.regex('caption'))
-async def caption(bot,update):
+async def caption(bot, update):
     text = script.CAPTION_TXT
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("🔙 Back",callback_data = "help")]
-		  ])
-    await update.message.edit(text = text,reply_markup = keybord)
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔙 Back", callback_data="help")]
+    ])
+    if update.message.text != text:
+        await update.message.edit_text(text=text, reply_markup=keybord)
 
 @Client.on_callback_query(filters.regex('donate'))
-async def donate(bot,update):
+async def donate(bot, update):
     text = script.DONATE_TXT
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("🔙 Back",callback_data = "help")]
-		  ])
-    await update.message.edit(text = text,reply_markup = keybord)
-
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔙 Back", callback_data="help")]
+    ])
+    if update.message.text != text:
+        await update.message.edit_text(text=text, reply_markup=keybord)
 
 @Client.on_callback_query(filters.regex('home'))
 async def home_callback_handler(bot, query):
     text = f"""{query.from_user.mention} \n<b>Je suis Yor Kingcey ||forger|| l'épouse de roi kingcey je suis un bot puissant capable de renommé n'importe quel type de fichier, comme je le fait avec mes proies.\n\n\n┣⪼<blockquote>||Remercier mon époux de m'avoir créé||</b></blockquote>"""
-    keybord = InlineKeyboardMarkup([  
-                    [InlineKeyboardButton("📢 Updates", url="https://telegram.me/BotZFlix"),
-                    InlineKeyboardButton("💬 Support", url="https://telegram.me/BotZFlixSupport")],
-                    [InlineKeyboardButton("🛠️ Aide", callback_data='help'),
-		            InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
-                    [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://telegram.me/kingcey")]
-		  ])
-    await query.message.edit_text(text=text, reply_markup=keybord)
+    keybord = InlineKeyboardMarkup([
+        [InlineKeyboardButton("📢 Updates", url="https://telegram.me/BotZFlix"),
+         InlineKeyboardButton("💬 Support", url="https://telegram.me/BotZFlixSupport")],
+        [InlineKeyboardButton("🛠️ Aide", callback_data='help'),
+         InlineKeyboardButton("❤️‍🩹 About", callback_data='about')],
+        [InlineKeyboardButton("🧑‍💻 Developer 🧑‍💻", url="https://telegram.me/kingcey")]
+    ])
+    if query.message.text != text:
+        await query.message.edit_text(text=text, reply_markup=keybord)
